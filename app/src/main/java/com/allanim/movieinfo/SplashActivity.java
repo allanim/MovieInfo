094febe8ca2c627b867a2d8b69895043b85bc823
Allan Im <allan@vogle.com>
2019-08-07 19:27:17 -0400

    Add Splash Activity

---
+++ app/src/main/java/com/allanim/movieinfo/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.allanim.movieinfo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(i);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+
+}
