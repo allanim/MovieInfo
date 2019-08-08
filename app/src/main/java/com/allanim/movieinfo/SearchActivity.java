ee9f307ee76f1137d92f8218ff6fc6daf06ca58e
Allan Im <allan@vogle.com>
2019-08-08 08:57:31 -0400

    Add Search Activity

---
+++ app/src/main/java/com/allanim/movieinfo/SearchActivity.java
@@ -0,0 +1,49 @@
+package com.allanim.movieinfo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.allanim.movieinfo.api.OmdbRepository;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        EditText mSearch = findViewById(R.id.search_name);
+        Button btnSearch = findViewById(R.id.btn_search);
+
+        btnSearch.setOnClickListener(v -> {
+            String keyword = mSearch.getText().toString();
+
+            // check valid
+            if (keyword.isEmpty()) {
+                mSearch.setError(getResources().getString(R.string.error_field_required));
+                mSearch.requestFocus();
+                return;
+            }
+
+            // search
+            OmdbRepository.getInstance().searchMovie(this, keyword, result -> {
+                if (result.getTotalResults() > 0) {
+                    Intent intent = new Intent(this, SearchActivity.class);
+                    intent.putExtra("searchList", result);
+                    startActivity(intent);
+                } else {
+                    // no data
+                    mSearch.setError(getResources().getString(R.string.no_result));
+                    mSearch.requestFocus();
+                }
+            });
+
+        });
+
+
+    }
+
+}
